b5f710c442746fb1748437f87da2ce13e7a4677d
Jared <jaredtrp@gmail.com>
2022-02-04 20:42:11 -0600

    created index.js

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,10 @@
+DROP DATABASE IF EXISTS corp;
+CREATE DATABASE corp;
+USE corp;
+
 DROP DATABASE IF EXISTS departments;
 DROP DATABASE IF EXISTS roles;
 DROP DATABASE IF EXISTS employees;
-
-CREATE DATABASE employee_db;
 
 CREATE TABLE departments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -11,17 +13,18 @@ CREATE TABLE departments (
 
 CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    role_name VARCHAR(30) NOT NULL,
-    role_salary DECIMAL(9,2) NOT NULL,
-    FOREIGN KEY (DepartmentID) REFERENCES departments(id)
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(8,2),
+    department_id INTEGER,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
 CREATE TABLE employees (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  job_title VARCHAR(30), NOT NULL,
-  emp_manager VARCHAR(30), NOT NULL,
-  FOREIGN KEY (DepartmentID) REFERENCES departments(id),
-  FOREIGN KEY (emp_salary) REFERENCES roles(role_salary)
+  role_id INTEGER,
+  FOREIGN KEY (role_id) REFERENCES roles(id),
+  manager_id INTEGER,
+  FOREIGN KEY (manager_id) REFERENCES employee(id),
 );
---
+++ index.js
@@ -0,0 +1,113 @@
+const inquirer = require('inquirer');
+const db = require('./db/connection');
+
+const promptUser = () => {
+  return inquirer
+    .prompt([
+      {
+        type: 'list',
+        message: 'Choose an option.',
+        name: 'action',
+        choices: [
+          'View all Departments',
+          'View all Roles',
+          'View all Employees',
+          'Add a Department',
+          'Add a Role',
+          'Add an Employee',
+          'Update an Employee Role',
+        ],
+      },
+    ])
+    .then(({ action }) => {
+      console.log(action);
+      switch (action) {
+        case 'View all Departments':
+          viewDepartments();
+          break;
+        case 'View all Roles':
+          viewRoles();
+          break;
+        case 'View all Employees':
+          viewHands();
+          break;
+        case 'Add a Department':
+          promptDepartment();
+          break;
+        case 'Add a Role':
+          promptRole();
+          break;
+        case 'Add an Employee':
+          promptEmployee();
+          break;
+        case 'Update an Employee Role':
+          updateRole();
+          break;
+      }
+    });
+};
+
+const viewDepartments = () => {
+  const sql = `SELECT * FROM department`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      // console.log(err);
+      return;
+    }
+    console.table(rows);
+    promptUser();
+  });
+};
+
+const viewRoles = () => {
+  const sql = `SELECT * FROM role`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      return err;
+    }
+    console.table(rows);
+    promptUser();
+  });
+};
+
+const viewHands = () => {
+  const sql = `SELECT * FROM employee;`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      return err;
+    }
+    console.table(rows);
+    promptUser();
+  });
+};
+
+const promptDepartment = () => {
+  return inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the department you would like to add?',
+      },
+    ])
+    .then(({ name }) => {
+      const sql = `INSERT INTO department (name) VALUES ('${name}')`;
+      // console.log(name);
+      db.query(sql, (err, rows) => {
+        if (err) {
+          console.log(err);
+          return err;
+        }
+        promptUser();
+      });
+    });
+};
+
+db.connect(err => {
+  if (err) throw err;
+  console.log('Database connected');
+  promptUser();
+});
